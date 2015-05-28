df2b1d9e17814d8cdc1010259191f7a8388510d1
Vincent DAVENEL <vincent_mil@hotmail.fr>
2015-05-28 16:51:22 +0200

    MAJ Arborecsence
    - mise a jour de l'arborenscence

---
+++ Controleur/ControleurUser.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'Framework/Controleur.php';
+require_once 'Modele/user.php';
+
+/**
+*
+* controleur de l'ojet user
+* effectuer toute les operations sur les objets user
+*
+**/
+
+class ControleurUser extends Controleur
+{
+	
+}
