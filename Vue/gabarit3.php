498b90b1e93b07c75d4163b0ed0a3b1659ccfd45
BetterCall <bryann@brovia.com>
2016-05-05 15:53:27 +0200

    Gabarit 3

--- Vue/gabarit3.php
+++ Vue/gabarit3.php
@@ -59,14 +59,14 @@
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <span class="hidden-xs">USERNAME</span>
+              <span class="hidden-xs">$username</span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
 
                 <p>
-                  USERNAME
+                  $username
 
                 </p>
               </li>
